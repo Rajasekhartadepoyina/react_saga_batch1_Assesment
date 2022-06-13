99e3d92600b88f5e5afb971e1d0ab9ac09ea89b6
Rajasekhar390 <sekharrajan390@gmail.com>
2022-06-13 07:58:24 +0530

    update

--- src/redux/sagas/dashboard_sagas.js
+++ src/redux/sagas/dashboard_sagas.js
@@ -1,44 +1,44 @@
-import {put,takeEvery} from 'redux-saga/effects'
+import { put, takeEvery } from 'redux-saga/effects'
 import { LoaderTypes } from '../action_types/loader_types'
-import {DashboardTypes} from '../action_types/dashboard_types'
+import { DashboardTypes } from '../action_types/dashboard_types'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
-function* dashboardData(){
-    yield put({type:LoaderTypes.LOADER_START})
+function* dashboardData() {
+    yield put({ type: LoaderTypes.LOADER_START })
 
-    try{
-        const url="https://reqres.in/api/users"
+    try {
+        const url = "https://reqres.in/api/users"
         // const url = `${process.env.REACT_APP_API_URL}/users`
-        const res=yield axios.get(url)
-        yield put({type:DashboardTypes.USER_DATA_REQUEST_SUCCESS,data: res.data.data})
-        console.log('dash_saga',res.data.data)
+        const res = yield axios.get(url)
+        yield put({ type: DashboardTypes.USER_DATA_REQUEST_SUCCESS, data: res.data.data })
+        console.log('dash_saga', res.data.data)
     }
-    catch(e){
+    catch (e) {
 
     }
-    yield put({ type: LoaderTypes.LOADER_STOP});
+    yield put({ type: LoaderTypes.LOADER_STOP });
 }
 //user request by id
-function* userRequest(data,callback){
- console.log('da',data.data.id);
-    yield put({type:LoaderTypes.LOADER_START})
+function* userRequest(data, callback) {
+    console.log('da', data.data.id);
+    yield put({ type: LoaderTypes.LOADER_START })
 
-    try{
-        const url=`https://reqres.in/api/users/${data.data.id}`
+    try {
+        const url = `https://reqres.in/api/users/${data.data.id}`
         // const url = `${process.env.REACT_APP_API_URL}/users`
-        const res=yield axios.get(url)
-        yield put({type:DashboardTypes.PERSON_REQUST_SUCCESS,data: res.data.data})
-        console.log('dash_saga',res.data.data)
+        const res = yield axios.get(url)
+        yield put({ type: DashboardTypes.PERSON_REQUST_SUCCESS, data: res.data.data })
+        console.log('dash_saga', res.data.data)
         callback(true);
     }
-    catch(e){
+    catch (e) {
 
     }
-    yield put({ type: LoaderTypes.LOADER_STOP});
+    yield put({ type: LoaderTypes.LOADER_STOP });
 }
 
-export default function* dashboardSaga(){
-    yield takeEvery(DashboardTypes.USER_DATA_REQUEST,dashboardData);
-    yield takeEvery(DashboardTypes.PERSON_REQUST,userRequest)
+export default function* dashboardSaga() {
+    yield takeEvery(DashboardTypes.USER_DATA_REQUEST, dashboardData);
+    yield takeEvery(DashboardTypes.PERSON_REQUST, userRequest)
 }
